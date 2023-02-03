c4f434435127a90c8dcb1a9ab749d96a43c99093
Judhi Prasetyo <judhi@judhi.net>
2023-02-03 20:56:00 +0400

    Update BLE_Key_Touch.ino
    Added MacOS & IOS CMD key support

--- BLE_Key_Touch.ino
+++ BLE_Key_Touch.ino
@@ -1,22 +1,24 @@
 /*  BLE Custom Macro Key Touch
- *  by Judhi Prasetyo 2023
- *  based on T-vK/ESP-BLE-Keyboard
- *  (https://github.com/T-vK/ESP32-BLE-Keyboard)
- *  
- *  This code is available on 
- *  https://github.com/judhi/BLE-MacroKey 
- */
+    by Judhi Prasetyo 2023
+    based on T-vK/ESP-BLE-Keyboard
+    (https://github.com/T-vK/ESP32-BLE-Keyboard)
+
+    This code is available on
+    https://github.com/judhi/BLE-MacroKey
+*/
 
 #define TouchPin1 T4
 #define TouchPin2 T5
 #define TouchPin3 T6
-#define TouchPin4 T7
+#define TouchPin4 T3
 
-const int TOUCH_THRESHOLD = 20;
+const int UPPER_THRESHOLD = 20;
+const int LOWER_THRESHOLD = 5;
 int t1, t2, t3, t4;
 
 #include <BleKeyboard.h>
 
+/* change the name below */
 BleKeyboard bleKeyboard("Judhi BLE-Touch", "Judhi", 100);
 
 void setup() {
@@ -24,6 +26,8 @@ void setup() {
   Serial.begin(115200);
   bleKeyboard.begin();
   pinMode(2, OUTPUT);
+  delay(500);
+  Serial.println("BLE Touch Macro ready!");
 }
 
 void loop() {
@@ -33,32 +37,38 @@ void loop() {
   t3 = touchRead(TouchPin3);
   t4 = touchRead(TouchPin4);
 
-  if (t1 < TOUCH_THRESHOLD) {
-    Serial.println("Touched 1");
-    bleKeyboard.press(KEY_LEFT_CTRL);
+  if (t1 > LOWER_THRESHOLD && t1 < UPPER_THRESHOLD) {
+    Serial.print(t1);
+    Serial.println(" Touched 1");
+    bleKeyboard.press(KEY_LEFT_CTRL); // Windows & Linux CTRL key
+    // bleKeyboard.press(KEY_LEFT_GUI);  // MacOS & IOS CMD key
     bleKeyboard.press('c');
     delay(100);
     bleKeyboard.releaseAll();
     delay(250);
   }
 
-  if (t2 < TOUCH_THRESHOLD) {
-    Serial.println("Touched 2");
-    bleKeyboard.press(KEY_LEFT_CTRL);
+  if (t2 > LOWER_THRESHOLD && t2 < UPPER_THRESHOLD) {
+    Serial.print(t2);
+    Serial.println(" Touched 2");
+    bleKeyboard.press(KEY_LEFT_CTRL); // Windows & Linux CTRL key
+    // bleKeyboard.press(KEY_LEFT_GUI);  // MacOS & IOS CMD key
     bleKeyboard.press('v');
     delay(100);
     bleKeyboard.releaseAll();
     delay(250);
   }
 
-  if (t3 < TOUCH_THRESHOLD) {
-    Serial.println("Touched 3");
+  if (t3 > LOWER_THRESHOLD && t3 < UPPER_THRESHOLD) {
+    Serial.print(t3);
+    Serial.println(" Touched 3");
     bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
     delay(250);
   }
 
-  if (t4 < TOUCH_THRESHOLD) {
-    Serial.println("Touched 4");
+  if (t4 > LOWER_THRESHOLD && t4 < UPPER_THRESHOLD) {
+    Serial.print(t4);
+    Serial.println(" Touched 4");
     bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
     delay(250);
   }
